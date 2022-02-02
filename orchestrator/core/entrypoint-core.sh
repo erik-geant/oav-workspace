da6965d4e5ee52d2088d0548ebd77932d60434fd
Erik Reid <erik.reid@geant.org>
2022-02-02 09:32:57 +0100

    use depends_on/condition to wait for postgres & redis

--- orchestrator/core/entrypoint-core.sh
+++ orchestrator/core/entrypoint-core.sh
@@ -4,13 +4,11 @@
 # redis container name: redis
 
 # orchestrator settings
-export DATABASE_URI=postgresql://$APP_DB_USER:$APP_DB_PASS@postgres/$APP_DB_NAME
+export DATABASE_URI=postgresql://$APP_DB_USER:$APP_DB_PASS@$APP_DB_HOSTNAME/$APP_DB_NAME
 export CACHE_HOST=redis
-export WEBSOCKET_BROADCASTER_URL=redis://nobody:$REDIS_PASSWORD@redis:6379
+export WEBSOCKET_BROADCASTER_URL=redis://nobody:$REDIS_PASSWORD@$REDIS_HOSTNAME:6379
 
 
-sleep 10
 python3 core.py db init
 python3 core.py db upgrade heads
-echo CORE_LOCAL_PORT: $CORE_LOCAL_PORT
 PYTHONPATH=/ uvicorn --reload --host 0.0.0.0 --port $CORE_LOCAL_PORT core:app
